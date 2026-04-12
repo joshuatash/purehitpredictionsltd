566de4d85e2df6e2c85c673d3a4492fada98cdd8
Okuta Joshua <okutajoshuatash256@gmail.com>
2026-04-12 20:24:37 +0300

    Update markets.js

--- markets.js
+++ markets.js
@@ -1,24 +1,24 @@
 const MARKET_DATA = {
     "O 1.5": [
         { date: "2026-04-12", time: "22:30", home: "Estroil", away: "FC Porto", prediction: "O 1.5", odds: "1.25", status: "pending" },
-        { date: "2026-04-12", time: "19:30", home: "Celta Vigo", away: "Oviedo", prediction: "O 1.5", odds: "1.32", status: "pending" },
+        { date: "2026-04-12", time: "19:30", home: "Celta Vigo", away: "Oviedo", prediction: "O 1.5-WON", odds: "1.32", status: "pending" },
         { date: "2026-04-12", time: "21:45", home: "Como", away: "Inter", prediction: "O 1.5", odds: "1.30", status: "pending" },
         { date: "2026-04-12", time: "19:00", home: "Bologna", away: "Lecce", prediction: "O 1.5", odds: "1.36", status: "pending" },
         { date: "2026-04-12", time: "21:45", home: "Lyon", away: "Lorient", prediction: "O 1.5", odds: "1.30", status: "pending" },
-        { date: "2026-04-12", time: "16:00", home: "Nottingham Forest", away: "Aston Villa", prediction: "O 1.5", odds: "1.31", status: "pending" },
-        { date: "2026-04-12", time: "16:00", home: "Crystal Palace", away: "Newcastle", prediction: "O 1.5", odds: "1.24", status: "pending" }
+        { date: "2026-04-12", time: "16:00", home: "Nottingham Forest", away: "Aston Villa", prediction: "O 1.5-WON", odds: "1.31", status: "pending" },
+        { date: "2026-04-12", time: "16:00", home: "Crystal Palace", away: "Newcastle", prediction: "O 1.5-WON", odds: "1.24", status: "pending" }
     ],
     "O 2.5": [
         { date: "2026-04-12", time: "20:00", home: "SC Braga", away: "Arouca", prediction: "O 2.5", odds: "1.67", status: "pending" },
-        { date: "2026-04-12", time: "17:45", home: "AZ Alkmaar", away: "Heerenveen", prediction: "O 2.5", odds: "1.45", status: "pending" },
-        { date: "2026-04-12", time: "15:30", home: "NEC Nijmegen", away: "Feyenoord", prediction: "O 2.5", odds: "1.46", status: "pending" },
+        { date: "2026-04-12", time: "17:45", home: "AZ Alkmaar", away: "Heerenveen", prediction: "O 2.5-WON", odds: "1.45", status: "pending" },
+        { date: "2026-04-12", time: "15:30", home: "NEC Nijmegen", away: "Feyenoord", prediction: "O 2.5-LOST", odds: "1.46", status: "pending" },
         { date: "2026-04-12", time: "18:30", home: "VfB Stuttgart", away: "Hamburger SV", prediction: "O 2.5", odds: "1.45", status: "pending" }
     ],
     "DC": [
-        { date: "2026-04-12", time: "16:00", home: "Parma", away: "Napoli", prediction: "12 DC", odds: "1.38", status: "pending" },
+        { date: "2026-04-12", time: "16:00", home: "Parma", away: "Napoli", prediction: "12 DC", odds: "1.38-LOST", status: "pending" },
         { date: "2026-04-12", time: "18:15", home: "Nice", away: "Le Havre", prediction: "1X DC", odds: "1.22", status: "pending" },
         { date: "2026-04-12", time: "18:30", home: "Chelsea", away: "Manchester City", prediction: "X2 DC", odds: "1.37", status: "pending" },
-        { date: "2026-04-12", time: "14:00", home: "Falkirk", away: "Rangers", prediction: "X2 DC", odds: "1.20", status: "pending" },
+        { date: "2026-04-12", time: "14:00", home: "Falkirk", away: "Rangers", prediction: "X2 DC-WON", odds: "1.20", status: "pending" },
         { date: "2026-04-12", time: "14:00", home: "Tapatio", away: "Irapuato", prediction: "12 DC", odds: "1.26", status: "pending" }
     ]
 };
