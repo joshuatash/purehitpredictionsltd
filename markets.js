6dd71b4d6f3514d50812d107056f598d967af3e1
Okuta Joshua <okutajoshuatash256@gmail.com>
2026-04-12 20:47:59 +0300

    Update markets.js

--- markets.js
+++ markets.js
@@ -1,12 +1,12 @@
 const MARKET_DATA = {
     "O 1.5": [
         { date: "2026-04-12", time: "22:30", home: "Estroil", away: "FC Porto", prediction: "O 1.5", odds: "1.25", status: "pending" },
-        { date: "2026-04-12", time: "19:30", home: "Celta Vigo", away: "Oviedo", prediction: "O 1.5-WON", odds: "1.32", status: "pending" },
+        { date: "2026-04-12", time: "19:30", home: "Celta Vigo", away: "Oviedo", prediction: "O 1.5", odds: "1.32-WON", status: "pending" },
         { date: "2026-04-12", time: "21:45", home: "Como", away: "Inter", prediction: "O 1.5", odds: "1.30", status: "pending" },
         { date: "2026-04-12", time: "19:00", home: "Bologna", away: "Lecce", prediction: "O 1.5", odds: "1.36", status: "pending" },
         { date: "2026-04-12", time: "21:45", home: "Lyon", away: "Lorient", prediction: "O 1.5", odds: "1.30", status: "pending" },
-        { date: "2026-04-12", time: "16:00", home: "Nottingham Forest", away: "Aston Villa", prediction: "O 1.5-WON", odds: "1.31", status: "pending" },
-        { date: "2026-04-12", time: "16:00", home: "Crystal Palace", away: "Newcastle", prediction: "O 1.5-WON", odds: "1.24", status: "pending" }
+        { date: "2026-04-12", time: "16:00", home: "Nottingham Forest", away: "Aston Villa", prediction: "O 1.5", odds: "1.31-WON", status: "pending" },
+        { date: "2026-04-12", time: "16:00", home: "Crystal Palace", away: "Newcastle", prediction: "O 1.5", odds: "1.24-WON", status: "pending" }
     ],
     "O 2.5": [
         { date: "2026-04-12", time: "20:00", home: "SC Braga", away: "Arouca", prediction: "O 2.5", odds: "1.67", status: "pending" },
