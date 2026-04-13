13627715607a736843e1e54384641b44f4de7f52
Okuta Joshua <okutajoshuatash256@gmail.com>
2026-04-13 06:10:26 +0300

    Update data.js

--- data.js
+++ data.js
@@ -6,15 +6,15 @@ const PURE_HIT_DATA = {
             home: "Galatasaray",
             away: "Kocaelispor",
             prediction: "BTTS",
-            odds: "2.20",
-            status: "pending"
+            odds: "2.20-WON",
+            status: "WON"
         }
     ],
     ordinary: [
-        { date: "2026-04-12", time: "20:00", home: "Twente", away: "FC Volendam", prediction: "Home Win", odds: "1.20", status: "pending" },
-        { date: "2026-04-12", time: "20:30", home: "Prijedor", away: "Borac Banja", prediction: "Away Win", odds: "1.50", status: "pending" },
-        { date: "2026-04-12", time: "20:45", home: "AS Roma", away: "Pisa", prediction: "Home Win", odds: "1.40", status: "pending" },
-        { date: "2026-04-12", time: "19:00", home: "Marseille", away: "Metz", prediction: "Home WIn", odds: "1.30", status: "pending" },
-        { date: "2026-04-12", time: "14:00", home: "Flora Tallinn", away: "Vaprus", prediction: "Home Win", odds: "1.33", status: "pending" }
+        { date: "2026-04-12", time: "20:00", home: "Twente", away: "FC Volendam", prediction: "Home Win", odds: "1.20-WON", status: "pending" },
+        { date: "2026-04-12", time: "20:30", home: "Prijedor", away: "Borac Banja", prediction: "Away Win", odds: "1.50-WON", status: "pending" },
+        { date: "2026-04-12", time: "20:45", home: "AS Roma", away: "Pisa", prediction: "Home Win", odds: "1.40-WON", status: "pending" },
+        { date: "2026-04-12", time: "19:00", home: "Marseille", away: "Metz", prediction: "Home WIn", odds: "1.30-WON", status: "pending" },
+        { date: "2026-04-12", time: "14:00", home: "Flora Tallinn", away: "Vaprus", prediction: "Home Win", odds: "1.33-WON", status: "pending" }
     ]
 };
