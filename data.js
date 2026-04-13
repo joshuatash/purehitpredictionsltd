c55989f72fa7444fbe782d9f17bfb2e1598e04c0
Okuta Joshua <okutajoshuatash256@gmail.com>
2026-04-13 13:44:48 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,20 +1,20 @@
 const PURE_HIT_DATA = {
     vip: [
         {
-            date: "2026-04-12",
-            time: "19:00",
-            home: "Galatasaray",
-            away: "Kocaelispor",
+            date: "2026-04-13",
+            time: "22:00",
+            home: "Levante",
+            away: "Getafe",
             prediction: "BTTS",
-            odds: "2.20-WON",
-            status: "WON"
+            odds: "2.23",
+            status: "pendind"
         }
     ],
     ordinary: [
-        { date: "2026-04-12", time: "20:00", home: "Twente", away: "FC Volendam", prediction: "Home Win", odds: "1.20-WON", status: "pending" },
-        { date: "2026-04-12", time: "20:30", home: "Prijedor", away: "Borac Banja", prediction: "Away Win", odds: "1.50-WON", status: "pending" },
-        { date: "2026-04-12", time: "20:45", home: "AS Roma", away: "Pisa", prediction: "Home Win", odds: "1.40-WON", status: "pending" },
-        { date: "2026-04-12", time: "19:00", home: "Marseille", away: "Metz", prediction: "Home WIn", odds: "1.30-WON", status: "pending" },
-        { date: "2026-04-12", time: "14:00", home: "Flora Tallinn", away: "Vaprus", prediction: "Home Win", odds: "1.33-WON", status: "pending" }
+        { date: "2026-04-13", time: "21:00", home: "Manchester United", away: "Leeds", prediction: "BTTS", odds: "1.90", status: "pending" },
+        { date: "2026-04-13", time: "21:00", home: "Jong Ajax", away: "FC OSS", prediction: "1X", odds: "1.40", status: "pending" },
+        { date: "2026-04-13", time: "20:00", home: "Raufoss", away: " Kongsvinger", prediction: "2", odds: "1.7", status: "pending" },
+        { date: "2026-04-13", time: "21:30", home: "Real Valladolid", away: "Eibar", prediction: "O 1.5", odds: "1.38", status: "pending" },
+        { date: "2026-04-13", time: "21:45", home: "ACF Fiorentina", away: "Lazio", prediction: "O 1.5", odds: "1.39", status: "pending" }
     ]
 };
